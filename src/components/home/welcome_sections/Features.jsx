2606bb3f5c46a76b8de26062a78748866d3670a7
gustavo-vasquez <vasquez.mgustavo@gmail.com>
2022-10-30 18:09:14 -0300

    Iconos para "features" guardados en local

--- src/components/home/welcome_sections/Features.jsx
+++ src/components/home/welcome_sections/Features.jsx
@@ -5,32 +5,32 @@ function Features() {
             <h1 className="display-5">Echa un vistazo a algunas de las características de la plataforma</h1>
             <div className="row">
                 <div className="col-12 col-md-4">
-                    <img className="img-fluid" width={150} src="https://previews.123rf.com/images/yupiramos/yupiramos1803/yupiramos180304203/96612758-galer%C3%ADa-de-fotos-%C3%A1lbum-digital-icono-ilustraci%C3%B3n-vectorial-dise%C3%B1o-de-dibujo.jpg" alt="galeria_icono"/>
+                    <img className="img-fluid" width={150} src="/img/features/galeria.png" alt="galeria_icono"/>
                     <h3>Galería de animales</h3>
                     <p className="text-muted">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
                 </div>
                 <div className="col-12 col-md-4">
-                    <img className="img-fluid" width={150} src="https://img.pikbest.com/png-images/make-tag-drawing-calendar-clipart_5856423.png!c1024wm0" alt="turnos_icono"/>
+                    <img className="img-fluid" width={150} src="/img/features/turnos.png" alt="turnos_icono"/>
                     <h3>Reserva de turnos</h3>
 		            <p className="text-muted">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
                 </div>
                 <div className="col-12 col-md-4">
-                    <img className="img-fluid" width={150} src="https://img.freepik.com/vector-premium/cachorro-triste-hogar-caja-adopta-al-perro-ilustracion-vectorial_560705-22.jpg?w=2000" alt="perro_en_la_calle" />
+                    <img className="img-fluid" width={150} src="/img/features/encontrar_animales.png" alt="perro_en_la_calle" />
                     <h3>Ayuda a encontrar animales extraviados</h3>
 		            <p className="text-muted">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
                 </div>
                 <div className="col-12 col-md-4">
-                    <img className="img-fluid" width={150} src="https://img.freepik.com/vector-gratis/cliente-personaje-dibujos-animados-plana-50-ciento-descuento-cupon_1150-34877.jpg?w=2000" alt="descuento_icono"/>
+                    <img className="img-fluid" width={150} src="/img/features/descuentos.png" alt="descuento_icono"/>
                     <h3>Descuentos</h3>
 		            <p className="text-muted">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
                 </div>
                 <div className="col-12 col-md-4">
-                    <img className="img-fluid" width={150} src="https://st2.depositphotos.com/1985863/5934/v/600/depositphotos_59341323-stock-illustration-business-woman-using-computer.jpg" alt="facil_de_usar"/>
+                    <img className="img-fluid" width={150} src="/img/features/facil_de_usar.jpg" alt="facil_de_usar"/>
                     <h3>Fácil de usar</h3>
                     <p className="text-muted">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
                 </div>
                 <div className="col-12 col-md-4">
-                    <img className="img-fluid" width={150} src="https://img.freepik.com/vector-premium/perro-dibujos-animados-corriendo-calle-ilustracion-colorida-brillante_88100-584.jpg" alt="perro_libre" />
+                    <img className="img-fluid" width={150} src="/img/features/gratuito.png" alt="perro_libre" />
                     <h3>Gratuito</h3>
 		            <p className="text-muted">All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
                 </div>
